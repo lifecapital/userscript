c6894102e748b18167d863506692fac921cc803a
Mustafa Elmalah <mnav@fastmail.com>
2024-10-02 17:23:22 +0300

    ipharmacy autorefresh userscript

---
+++ ipharmacy_autorefresh.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         ipharmacy_autorefresh
+// @namespace    http://lifecapital.eg
+// @version      0.4
+// @description  I-Pharmacy Autorefresh
+// @author       Mustafa Elmalah
+// @match 		*://portal.i-pharmacy.app/*
+// @run-at document-start
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/lifecapital/userscript/refs/heads/master/ipharmacy_autorefresh.user.js
+// @updateURL    https://raw.githubusercontent.com/lifecapital/userscript/refs/heads/master/ipharmacy_autorefresh.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+	let id = null;
+
+	const proc = () => {
+		clearTimeout(id);
+		window.location.reload();
+		id = setTimeout(proc, 30000);
+	};
+
+	proc();
+
+})();
